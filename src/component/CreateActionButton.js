be76d02645bd6c9a93c70cd6ad2ff580f78d627e
jiseong <jisung87kr@nate.com>
2021-08-28 16:42:35 +0900

    스타일 수정

--- src/component/CreateActionButton.js
+++ src/component/CreateActionButton.js
@@ -1,7 +1,7 @@
 const CreateActionButton = ({onSubmit, onCancle}) => {
     return(
         <div>
-            <button className="btn btn-primary" onClick={onSubmit}>submit</button>
+            <button className="btn btn-primary mx-2" onClick={onSubmit}>submit</button>
             <button className="btn btn-secondary" onClick={onCancle}>back</button>
         </div>
     )
